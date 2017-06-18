9f1e16193603582e789c657a94a2019f447ed13a
Travis Brown <travisrobertbrown@gmail.com>
2017-06-18 14:01:45 -0500

    Split some tests out of tests project

---
+++ src/test/scala/io/circe/streaming/StreamingSuite.scala
@@ -0,0 +1,48 @@
+package io.circe.streaming
+
+import cats.Eval
+import cats.data.EitherT
+import io.circe.{ Decoder, Encoder }
+import io.circe.syntax._
+import io.circe.tests.CirceSuite
+import io.circe.tests.examples._
+import io.iteratee.{ Enumeratee, Enumerator }
+import io.iteratee.modules.eitherT._
+
+class StreamingSuite extends CirceSuite {
+  type Result[A] = EitherT[Eval, Throwable, A]
+
+  implicit val decodeFoo: Decoder[Foo] = Foo.decodeFoo
+  implicit val encodeFoo: Encoder[Foo] = Foo.encodeFoo
+
+  def enumerateFoos(fooStream: Stream[Foo], fooVector: Vector[Foo]): Enumerator[Result, Foo] =
+    enumStream(fooStream).append(enumVector(fooVector))
+
+  def serializeFoos(foos: Enumerator[Result, Foo]): Enumerator[Result, String] =
+    enumOne("[").append(foos.through(map((_: Foo).asJson.spaces2).andThen(intersperse(", ")))).append(enumOne("]"))
+
+  def stringToBytes: Enumeratee[Result, String, Array[Byte]] = map(
+    _.getBytes(java.nio.charset.Charset.forName("UTF-8"))
+  )
+
+  "stringParser" should "parse enumerated lines" in forAll { (fooStream: Stream[Foo], fooVector: Vector[Foo]) =>
+    val enumerator = serializeFoos(enumerateFoos(fooStream, fooVector))
+    val foos = (fooStream ++ fooVector).map(_.asJson)
+
+    assert(enumerator.through(stringParser).toVector.value.value === Right(foos.toVector))
+  }
+
+  "byteParser" should "parse enumerated bytes" in forAll { (fooStream: Stream[Foo], fooVector: Vector[Foo]) =>
+    val enumerator = serializeFoos(enumerateFoos(fooStream, fooVector)).through(stringToBytes)
+    val foos = (fooStream ++ fooVector).map(_.asJson)
+
+    assert(enumerator.through(byteParser).toVector.value.value === Right(foos.toVector))
+  }
+
+  "decoder" should "decode enumerated JSON values" in forAll { (fooStream: Stream[Foo], fooVector: Vector[Foo]) =>
+    val enumerator = serializeFoos(enumerateFoos(fooStream, fooVector))
+    val foos = fooStream ++ fooVector
+
+    assert(enumerator.through(stringParser).through(decoder[Result, Foo]).toVector.value.value === Right(foos.toVector))
+  }
+}
